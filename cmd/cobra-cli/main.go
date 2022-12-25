002f24f769381baf72fc726a1669a11be61700e8
Douglas Danger Manley <doug.manley@gmail.com>
2022-12-25 17:41:37 -0500

    Start the CLI

---
+++ cmd/cobra-cli/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"os"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	verbose := false
+	var rootCommand = &cobra.Command{
+		Use:   "cobra-cli",
+		Short: "Command-line tools for Cobra Controls access systems",
+		Long:  ``,
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+			if verbose {
+				logrus.SetLevel(logrus.DebugLevel)
+			}
+		},
+		Run: func(cmd *cobra.Command, args []string) {
+			cmd.Help()
+			os.Exit(1)
+		},
+	}
+	rootCommand.PersistentFlags().BoolVar(&verbose, "verbose", false, "Enable verbose output")
+
+}
